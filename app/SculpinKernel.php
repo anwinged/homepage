54e7e138761c6b536b73459cf60c14a3d5708d67
Anton Vakhrushev <anwinged@ya.ru>
2018-06-30 09:31:26 +0300

    Add site map

---
+++ app/SculpinKernel.php
@@ -0,0 +1,14 @@
+<?php
+
+use Nickpeirson\Sculpin\Bundle\SitemapBundle\SculpinSitemapBundle;
+use Sculpin\Bundle\SculpinBundle\HttpKernel\AbstractKernel;
+
+class SculpinKernel extends AbstractKernel
+{
+    protected function getAdditionalSculpinBundles()
+    {
+        return [
+            SculpinSitemapBundle::class,
+        ];
+    }
+}
