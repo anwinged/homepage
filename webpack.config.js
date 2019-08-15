61ebb7dae595363152cf57d08ac85bfe524698bd
Anton Vakhrushev <anwinged@ya.ru>
2019-08-15 15:46:32 +0300

    Добавлен рантайм-рансформ для babel

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,10 @@ module.exports = (env = {}) => {
         loader: 'babel-loader',
         options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
+            plugins: [
+                '@babel/plugin-transform-runtime',
+                '@babel/plugin-proposal-class-properties',
+            ],
         },
     };
 
