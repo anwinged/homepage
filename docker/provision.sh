872f5d22fe7e186cff0e208c0690c28e1f802ef9
Anton Vakhrushev <anwinged@ya.ru>
2020-03-18 19:18:54 +0300

    Исправил provision.sh

--- docker/provision.sh
+++ docker/provision.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env sh
 
-RUN apt-get update && apt-get install -y \
-    libtidy-dev \
+set -eux
+
+apt-get update && apt-get install -y \
     curl \
     git \
+    gnupg \
     gzip \
-    rsync
+    libtidy-dev \
+    rsync \
+    zip \
+;
 
-RUN docker-php-ext-install tidy \
+docker-php-ext-install tidy \
     && docker-php-ext-enable tidy
 
 # Project folder
-RUN mkdir -p /srv/app
+mkdir -p /srv/app
 
 WORKDIR /srv/app
 
-RUN apt-get update \
-    && apt-get install -y git zip curl gnupg \
-    && rm -rf /var/lib/apt/lists/*
-
 # Composer and required tools
-RUN curl -sLO https://getcomposer.org/download/1.9.3/composer.phar \
+curl -sLO https://getcomposer.org/download/1.9.3/composer.phar \
     && mv composer.phar /usr/local/bin/composer \
     && chmod +x /usr/local/bin/composer
 
 # Deployer
-RUN curl -sLO https://deployer.org/releases/v6.7.3/deployer.phar \
+curl -sLO https://deployer.org/releases/v6.7.3/deployer.phar \
     && mv deployer.phar /usr/local/bin/dep \
     && chmod +x /usr/local/bin/dep
 
