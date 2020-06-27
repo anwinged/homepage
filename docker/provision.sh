43cae05b76c5d5d4b1757508587bb9916bb8b326
Anton Vakhrushev <anwinged@ya.ru>
2020-06-27 19:46:26 +0300

    Обновил зависимости

--- docker/provision.sh
+++ docker/provision.sh
@@ -19,11 +19,11 @@ docker-php-ext-install tidy \
 mkdir -p /srv/app
 
 # Composer and required tools
-curl -sLO https://getcomposer.org/download/1.10.1/composer.phar \
+curl -sLO https://getcomposer.org/download/1.10.8/composer.phar \
     && mv composer.phar /usr/local/bin/composer \
     && chmod +x /usr/local/bin/composer
 
 # Deployer
-curl -sLO https://deployer.org/releases/v6.7.3/deployer.phar \
+curl -sLO https://deployer.org/releases/v6.8.0/deployer.phar \
     && mv deployer.phar /usr/local/bin/dep \
     && chmod +x /usr/local/bin/dep
